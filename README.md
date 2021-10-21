897ab565b3c2b155f630cebc148ee8aa69e77ef5
Jane Lu <jyl.janelu@gmail.com>
2021-10-20 23:43:49 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # MERN-book-search-engine
+
+![github license](https://img.shields.io/badge/license-MIT-blueviolet.png)
+
+For license link, please check at (https://choosealicense.com/licenses/mit/#)
+
+## Table Of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Questions](#questions)
+
+## Description
+
+This application is a online bookstore. Users can find and save their favorite books. The app is created with MERN stack, using React for the front end and MongoDB for the database.
+
+heroku:
+
+https://book-search-engine-janelu.herokuapp.com/
+
+## Installation
+
+- npm init
+- React
+- Express
+
+* Apollo server express
+* Graphql
+
+## Usage
+
+After installation, users can run npm start to connect
+
+## License
+
+This project is licensed under the MIT license.
+
+Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Contribution
+
+Jane Lu
+
+## Test
+
+no test required
+
+## Questions
+
+- For further information, please send email to jyl.janelu@gmail.com
+- Check for details on Github at [JYL-LU](https://github.com/JYL-LU)
--- server/server.js
+++ server/server.js
@@ -1,29 +1,77 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require("./utils/auth");
+// const express = require("express");
+// const { ApolloServer } = require("apollo-server-express");
+// const { authMiddleware } = require("./utils/auth");
 
+// const path = require("path");
+// const db = require("./config/connection");
+// //const routes = require("./routes");
+
+// //import our typeDefs and resolvers
+// const { typeDefs, resolvers } = require("./schemas");
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// const serverConfig = {
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// };
+
+// //create new Apollo server and pass in our schema data
+// const server = new ApolloServer(serverConfig);
+
+// server.applyMiddleware({ app });
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// //app.use(routes);
+
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
+const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-//const routes = require("./routes");
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+console.log(ApolloServer);
 
-//import our typeDefs and resolvers
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-//create new Apollo server and pass in our schema data
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+// integrate our Apollo server with the express application as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
@@ -32,8 +80,6 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-//app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
